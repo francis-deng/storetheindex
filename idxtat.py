bdc78335adf7863c4b01dea94a7665fa3c5d5fb3
francis_xiiiv <francis_xiiiv@163.com>
2022-07-04 00:46:51 -0700

    calculate the ingestion simply

--- idxtat.py
+++ idxtat.py
@@ -12,7 +12,7 @@ def parse_args():
     
     indices_parser = subparsers.add_parser('indices')
 
-    indices_parser.add_argument('--filter','-f',dest='filter',type=str,help='time span filter: [today,yesterday,thisweek,lastweek,thismonth,lastmonth]')
+    indices_parser.add_argument('--span','-s',dest='filter',type=str,help='time span filter: [today,yesterday,thisweek,lastweek,thismonth,lastmonth]')
     indices_parser.add_argument('--files','-f',dest='files',type=str,help='indexer log file',action='append')
 
     args = parser.parse_args()
@@ -30,7 +30,7 @@ class TimeSpanEnum(enum.Enum):
 class TimeSpan:
     def __init__(self, tsEnum):
         now = date.today()
-        if tsEnum == TimeSpanEnum.LastWeek:
+        if TimeSpanEnum(tsEnum) is TimeSpanEnum.LastWeek:
             s = now - timedelta(days=now.weekday()+7)
             f = now - timedelta(days=now.weekday()+1)
 
@@ -40,7 +40,7 @@ class TimeSpan:
             raise TypeError('cannot recognize TimeSpanEnum type')
 
     def __ge__(self, ts):
-        return self.tsStart<=ts && self.tsFinish>=ts
+        return self.tsStart<=ts and self.tsFinish>=ts
 
 def open_file_and_read_lines(file, filter):
     span = TimeSpan(filter)
@@ -53,7 +53,7 @@ def open_file_and_read_lines(file, filter):
             ## 2022-06-17T04:04:01.031Z        INFO    indexer/ingest  ingest/linksystem.go:359        Put multihashes in entry chunk  {"publisher": "12D3KooWNTSFywHjGbmGN1aEqJNp54pDKaiwqpthRrvFZETo37pW", "adCid": "baguqeera4x263k2uwwzpqolzx7eipe3ejvrliawqorwcko2dj5dqucvzxkza", "count": 5851}
             m = re.match('(^[0-9]{4}-[0-9]{2}-[0-9]{2}).+Put.+count\"\:\s(\d+)\}$', line)
             #print(len(m.groups()))
-            if len(m.groups()) == 2:
+            if m is not None and len(m.groups()) == 2:
                 #print(m.group(1))
                 #print(m.group(2))
                 record_datetime = datetime.fromisoformat(m.group(1))
@@ -67,29 +67,14 @@ def open_file_and_read_lines(file, filter):
 
     return count
 
-## egrep "2022-06-17T|2022-06-18T|2022-06-19T|2022-06-20T|2022-06-21T|2022-06-22T|2022-06-23T|2022-06-24T|2022-06-25T|2022-06-26T" indexd.out|grep Put|grep count>20220627.rec
+
+## python3 idxtat.py indices -s lastweek -f ./indexd.out.20220704 -f ./indexd.out.20220620
 if __name__ == '__main__':
     args = parse_args()
     count = 0
 
-    for file in args.file:
+    for file in args.files:
         count += open_file_and_read_lines(file, args.filter)
-'''
-    count = 0
-    if len(args.files)>0:
-        with open(args.file) as f:
-            for line in f:
-                #print('line=', line)
 
-                ## 2022-06-17T04:04:01.031Z        INFO    indexer/ingest  ingest/linksystem.go:359        Put multihashes in entry chunk  {"publisher": "12D3KooWNTSFywHjGbmGN1aEqJNp54pDKaiwqpthRrvFZETo37pW", "adCid": "baguqeera4x263k2uwwzpqolzx7eipe3ejvrliawqorwcko2dj5dqucvzxkza", "count": 5851}
-                m = re.match('(^[0-9]{4}-[0-9]{2}-[0-9]{2}).+Put.+count\"\:\s(\d+)\}$', line)
-                #print(len(m.groups()))
-                if len(m.groups()) == 2:
-                  #print(m.group(1))
-                  #print(m.group(2))
-                    try:
-                      count = count + int(m.group(2))
-                    except:
-                      println("incorrect digit: ",m.group(2))
-'''
     print(count)
+
